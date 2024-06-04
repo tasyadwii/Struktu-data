9885e48dabbdc466c69f0137da687c74bda0eacf
tasyadwii <145946797+tasyadwii@users.noreply.github.com>
2024-06-04 11:47:23 +0800

    Add files via upload

---
+++ meet8.py
@@ -0,0 +1,28 @@
+# Belajar Percabangan
+
+nama = input("masukan nama")
+
+# if kondisi : aksi(standar)
+#if nama=="ady": print("dosen undikma")
+#print ("program selesai")
+
+# if kondisi : aksi (IDENTITAS)
+
+#if nama=="edy":
+
+ #   print("Dosen undikma")
+  #  print("Dosen struktur data")
+#print ("Program selesai")
+
+    if nama=="edy":
+    print("Dosen Ganteng")
+    print(f"Dosen ganteng bgt, makasih nama")
+    
+else:
+    print("ah gak ganteng")
+ print(f"program selesai, thanks nama")
+
+
+
+
+
