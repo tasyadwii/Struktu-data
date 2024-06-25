e772cb62f578563c14c7b93f41f93656a8399edc
tasyadwii <145946797+tasyadwii@users.noreply.github.com>
2024-06-25 12:01:43 +0800

    Add files via upload

---
+++ meet10-11.py
@@ -0,0 +1,33 @@
+# Belajar perulangan (loop)
+
+bil = 1
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+bil = bil + 2
+print(bil)
+
+#for kondisi:
+#  aksi 
+
+# pengguna loop pada pemograman lain 
+#for (int)i ; i<=10; i++) {
+#    aksi
+#    aksi 
+#}
+
+bil = [0,1,3,4]
+print(bil)
+
+for i in bil :
+    print(bil)
+
+    #for bil in range(10)
+    #aksi
+    #aksi
+for i in bil2 :
+     # print(f"nilai i sekarang -> {i}")
+     print("pak edi ganteng")
+print("akhir program 2")
